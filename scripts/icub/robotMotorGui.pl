c69d046505d1965d10835033363c1229b82810a8
Florian Lance <lance.florian@gmail.com>
2014-07-28 15:51:08 +0200

    [scripts icub]
    * add emotionInterface
    * add robotMotorGui
    * add simFaceExpression

--- scripts/icub/robotMotorGui.pl
+++ scripts/icub/robotMotorGui.pl
@@ -10,14 +10,11 @@ our $PBase         = $CurrentDirectory . "/../../";
 our $PThird_party  = $PBase . "swooz-3rdparty/";
 
 $ENV{PATH} ="";
-
 $ENV{PATH} = $PThird_party . "ace/ace-6.1.0/bin;" . $ENV{PATH};
 $ENV{PATH} = $PThird_party . "gtk/bin;" . $ENV{PATH};
-$ENV{PATH} = $PThird_party . "opencv/opencv-2.49/build/bin/Release;" . $ENV{PATH};
-$ENV{PATH} = $PThird_party . "openni/openni-1.5.4.0/Lib;" . $ENV{PATH};
-$ENV{PATH} = $CurrentDirectory . "/../icub/bin;" . $ENV{PATH};
 
+$ENV{ICUB_ROOT} = ".";
 
 chdir "bin";
-system("gyarpmanager.exe");
+system("robotMotorGui.exe");
 chdir "..";
---
+++ scripts/icub/simFaceExpressions.pl
@@ -0,0 +1,19 @@
+
+
+use strict;
+use warnings;
+use Cwd;
+
+my $CurrentDirectory = cwd();
+
+our $PBase         = $CurrentDirectory . "/../../";
+our $PThird_party  = $PBase . "swooz-3rdparty/";
+
+$ENV{PATH} ="";
+$ENV{PATH} = $PThird_party . "ace/ace-6.1.0/bin;" . $ENV{PATH};
+
+$ENV{ICUB_ROOT} = ".";
+
+chdir "bin";
+system("simFaceExpressions.exe");
+chdir "..";
