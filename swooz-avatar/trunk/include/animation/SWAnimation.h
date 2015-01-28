c6795928d7229d5720905d9b694c7fc8624cfbf2
Florian Lance <lance.florian@gmail.com>
2015-01-28 17:50:33 +0100

    [licence]

--- swooz-avatar/trunk/include/animation/SWAnimation.h
+++ swooz-avatar/trunk/include/animation/SWAnimation.h
@@ -1,3 +1,40 @@
+
+
+/*******************************************************************************
+**                                                                            **
+**  SWoOz is a software platform written in C++ used for behavioral           **
+**  experiments based on interactions between people and robots               **
+**  or 3D avatars.                                                            **
+**                                                                            **
+**  This program is free software: you can redistribute it and/or modify      **
+**  it under the terms of the GNU Lesser General Public License as published  **
+**  by the Free Software Foundation, either version 3 of the License, or      **
+**  (at your option) any later version.                                       **
+**                                                                            **
+**  This program is distributed in the hope that it will be useful,           **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of            **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             **
+**  GNU Lesser General Public License for more details.                       **
+**                                                                            **
+**  You should have received a copy of the GNU Lesser General Public License  **
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.           **
+**                                                                            **
+** *****************************************************************************
+**          Authors: Guillaume Gibert, Florian Lance                          **
+**  Website/Contact: http://swooz.free.fr/                                    **
+**       Repository: https://github.com/GuillaumeGibert/swooz                 **
+********************************************************************************/
+
+
+
+/**
+ * \file SWAnimation.h
+ * \brief defines SWAnimation class
+ * \author Florian Lance
+ * \date 07/08/14
+ */
+
+
 #ifndef SWANIMATION_H
 #define SWANIMATION_H
 
@@ -8,6 +45,9 @@
 namespace swAnimation
 {
 
+    /**
+     * @brief The SWMod class
+     */
     class SWMod
     {
         public :
@@ -24,6 +64,9 @@ namespace swAnimation
 
     };
 
+    /**
+     * @brief The SWSeq class
+     */
     class SWSeq
     {
         public :
@@ -33,7 +76,9 @@ namespace swAnimation
             std::vector<std::vector<float> > m_rigidMotion;
     };
 
-
+    /**
+     * @brief The SWMsh class
+     */
     class SWMsh
     {
         public :
@@ -43,6 +88,9 @@ namespace swAnimation
     };
 
 
+    /**
+     * @brief The SWAnimation class
+     */
     class SWAnimation
     {
         public :
--- swooz-examples/trunk/kinect_data_loader_main.cpp
+++ swooz-examples/trunk/kinect_data_loader_main.cpp
@@ -1,4 +1,27 @@
-
+/*******************************************************************************
+**                                                                            **
+**  SWoOz is a software platform written in C++ used for behavioral           **
+**  experiments based on interactions between people and robots               **
+**  or 3D avatars.                                                            **
+**                                                                            **
+**  This program is free software: you can redistribute it and/or modify      **
+**  it under the terms of the GNU Lesser General Public License as published  **
+**  by the Free Software Foundation, either version 3 of the License, or      **
+**  (at your option) any later version.                                       **
+**                                                                            **
+**  This program is distributed in the hope that it will be useful,           **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of            **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             **
+**  GNU Lesser General Public License for more details.                       **
+**                                                                            **
+**  You should have received a copy of the GNU Lesser General Public License  **
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.           **
+**                                                                            **
+** *****************************************************************************
+**          Authors: Guillaume Gibert, Florian Lance                          **
+**  Website/Contact: http://swooz.free.fr/                                    **
+**       Repository: https://github.com/GuillaumeGibert/swooz                 **
+********************************************************************************/
 
 /**
  * \file kinect_data_loader_main.cpp
